012aa2a74f15bc2d8d71e9f53fed6647b292d71e
Md. Rifat Hasan Jihan <31377578+RHJihan@users.noreply.github.com>
2025-12-25 21:55:58 +0600

    feat: eagerly load order addresses using EntityGraph in OrderRepository

--- src/main/java/com/monomart/repository/OrderRepository.java
+++ src/main/java/com/monomart/repository/OrderRepository.java
@@ -4,11 +4,22 @@ import com.monomart.entities.Order;
 import com.monomart.entities.enums.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @EntityGraph(attributePaths = { "address" })
     Page<Order> findByUserId(Long userId, Pageable pageable);
+
+    @EntityGraph(attributePaths = { "address" })
     Page<Order> findByStatus(OrderStatus status, Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = { "address" })
+    Page<Order> findAll(Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = { "address" })
+    Optional<Order> findById(Long id);
 }
-
-
